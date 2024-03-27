3e7bf4f8926f31d1c7cd48f9b59296691fe3c1f1
winterrdog <winterrdog@protonmail.ch>
2024-03-27 12:48:12 +0300

    modularized code

--- cpp/hello-crow/rest-api.cc
+++ cpp/hello-crow/rest-api.cc
@@ -4,32 +4,34 @@
 
 using json = crow::json::wvalue;
 
+json hi_json(void) {
+  json res = {{"name", "data-is-mine"}, {"pi", 3.14}, {"cpp", true}};
+  return res;
+}
+
+json json_again(void) {
+  json res = {
+      {"name", "data-is-mine"},
+      {"pi", 3.14},
+      {"cpp", true},
+      {"array", json::list{{1, 2, 3}}},
+      {"object", json::object{{"key", "value"}}},
+  };
+  return res;
+}
+
+void catch_all(crow::response &res) {
+  res.body = "that url was not found";
+  res.end();
+}
+
 int main(void) {
   crow::SimpleApp app{};
 
   // routes
-  CROW_ROUTE(app, "/hi-json")
-  ([]() -> json {
-    json res = {{"name", "data-is-mine"}, {"pi", 3.14}, {"cpp", true}};
-    return res;
-  });
-  CROW_ROUTE(app, "/json-again")
-  ([]() -> json {
-    json res = {
-        {"name", "data-is-mine"},
-        {"pi", 3.14},
-        {"cpp", true},
-        {"array", json::list{{1, 2, 3}}},
-        {"object", json::object{{"key", "value"}}},
-    };
-    return res;
-  });
-
-  CROW_CATCHALL_ROUTE(app)
-  ([](crow::response &res) -> void {
-    res.body = "that url was not found";
-    res.end();
-  });
+  CROW_ROUTE(app, "/hi-json")(hi_json);
+  CROW_ROUTE(app, "/json-again")(json_again);
+  CROW_CATCHALL_ROUTE(app)(catch_all);
 
   // server has go to listen
   auto a{app.port(9090).run_async()};
