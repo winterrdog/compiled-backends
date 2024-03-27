4b29387206680abfa8a6e375db5af4e59d34452a
winterrdog <winterrdog@protonmail.ch>
2024-03-27 11:37:52 +0300

    the initiation

---
+++ cpp/hello-crow/init.cc
@@ -0,0 +1,21 @@
+#include <crow/crow.h>
+#include <crow/crow/app.h>
+#include <crow/crow/http_response.h>
+
+int main(void) {
+  // init app
+  crow::SimpleApp app{};
+
+  // routes
+  CROW_ROUTE(app, "/")([]() { return "Hello world"; });
+  CROW_CATCHALL_ROUTE(app)
+  ([](crow::response &res) {
+    res.body = "That URL..! doesn't seem to be here";
+    res.end();
+  });
+
+  // make server listen
+  auto a = app.port(9090).run_async();
+
+  return 0;
+}
